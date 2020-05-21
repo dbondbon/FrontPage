c61985f522dd21df7c9f10f9ce0d8c93248dbd68
& <1669691816@qq.com>
2020-05-21 14:50:41 +0800

    生产实习 页面一

---
+++ ProductionInternshipPage/work1/js/jld.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	charts.init({ id: 793, container: "jld_map", option: {} });
+	charts.init({ id: 1163, container: "jld_chart1", option: {} });
+	charts.init({ id: 1725, container: "jld_chart2", option: {} });
+	charts.init({ id: 1719, container: "jld_chart3", option: {} });
+	charts.init({ id: 1719, container: "jld_chart4", option: {} });
+	charts.init({ id: 1719, container: "jld_chart5", option: {} });
+});
