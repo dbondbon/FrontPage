33da412e4164ce1a48e20838dbbc233d5ecec4c2
& <1669691816@qq.com>
2020-05-26 16:45:22 +0800

    生产实习 页面四

--- ProductionInternshipPage/work1/js/jld.js
+++ ProductionInternshipPage/work1/js/jld.js
@@ -3,6 +3,4 @@ $(document).ready(function () {
 	charts.init({ id: 1163, container: "jld_chart1", option: {} });
 	charts.init({ id: 1725, container: "jld_chart2", option: {} });
 	charts.init({ id: 1719, container: "jld_chart3", option: {} });
-	charts.init({ id: 1719, container: "jld_chart4", option: {} });
-	charts.init({ id: 1719, container: "jld_chart5", option: {} });
 });
---
+++ ProductionInternshipPage/work1/js/swc.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	charts.init({ id: 1159, container: "swc_chart1", option: {} });
+	charts.init({ id: 1676, container: "swc_chart2", option: {} });
+	charts.init({ id: 1578, container: "swc_chart3", option: {} });
+	charts.init({ id: 514, container: "swc_chart4", option: {} });
+	charts.init({ id: 1087, container: "swc_chart5", option: {} });
+	charts.init({ id: 1457, container: "swc_chart6", option: {} });
+	charts.init({ id: 1640, container: "swc_chart7", option: {} });
+	charts.init({ id: 204, container: "swc_chart8", option: {} });
+});
