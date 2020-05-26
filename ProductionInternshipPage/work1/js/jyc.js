9b606ba6a80f62feb67d0d8ff881dfad1419b7e1
& <1669691816@qq.com>
2020-05-26 16:05:17 +0800

    生产实习 页面三

---
+++ ProductionInternshipPage/work1/js/jyc.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	charts.init({ id: 1721, container: "jyc_chart1", option: {} });
+	charts.init({ id: 1640, container: "jyc_chart2", option: {} });
+	charts.init({ id: 1456, container: "jyc_chart3", option: {} });
+	charts.init({ id: 1056, container: "jyc_chart4", option: {} });
+	charts.init({ id: 514, container: "jyc_chart5", option: {} });
+	charts.init({ id: 1087, container: "jyc_chart6", option: {} });
+	charts.init({ id: 1725, container: "jyc_chart7", option: {} });
+	charts.init({ id: 559, container: "jyc_chart8", option: {} });
+});
