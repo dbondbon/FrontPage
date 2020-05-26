c9df90b76e74ae74399cfc13f96c1b367b26b3c1
& <1669691816@qq.com>
2020-05-26 15:19:10 +0800

    生产实习 页面二

--- ProductionInternshipPage/work1/js/glc.js
+++ ProductionInternshipPage/work1/js/glc.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
 	charts.init({ id: 1721, container: "jld_chart1", option: {} });
 	charts.init({ id: 1725, container: "jld_chart2", option: {} });
 	charts.init({ id: 515, container: "jld_chart3", option: {} });
+	charts.init({ id: 1572, container: "glc_chart4", option: {} });
+	charts.init({ id: 629, container: "glc_chart5", option: {} });
+	charts.init({ id: 1056, container: "glc_chart6", option: {} });
 });
